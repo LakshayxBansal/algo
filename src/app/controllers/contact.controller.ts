ebde37abc82b764800595c165c385f22bb670fe6
harshalgofast <harsh.khandelwal@algofast.in>
2024-08-12 11:12:30 +0530

    Updated modification form and implemented multi-error feature

--- src/app/controllers/contact.controller.ts
+++ src/app/controllers/contact.controller.ts
@@ -1,56 +1,130 @@
-"use server"
+"use server";
 
-import * as zs from '../zodschema/zodschema';
-import {contactSchemaT} from '../models/models';
-import {createContactDB} from '../services/contact.service';
-import { getSession } from '../services/session.service';
-import {getContactList, getContactDetailsById} from '@/app/services/contact.service';
-import { SqlError } from 'mariadb';
+import * as zs from "../zodschema/zodschema";
+import { contactSchemaT } from "../models/models";
+import { createContactDB, updateContactDB } from "../services/contact.service";
+import { getSession } from "../services/session.service";
+import {
+  getContactList,
+  getContactDetailsById,
+} from "@/app/services/contact.service";
+import { SqlError } from "mariadb";
 
-
-
-export async function createContact(data: contactSchemaT){
-
+export async function createContact(data: contactSchemaT) {
   let result;
   try {
     const session = await getSession();
-    if(session){
+    if (session) {
       const parsed = zs.contactSchema.safeParse(data);
-      if(parsed.success) {
+      if (parsed.success) {
         const dbResult = await createContactDB(session, data as contactSchemaT);
-        if (dbResult.length >0 && dbResult[0][0].error === 0) {
-          result = {status: true, data:dbResult[1]};
-         } else {
-          result = {status: false, data: [{path:[dbResult[0][0].error_path], message:dbResult[0][0].error_text}] };
-         }
-      } else {
-        let errorState: {path: (string | number)[], message: string}[] = [];
-        for (const issue of parsed.error.issues) {
-          errorState.push({ path: issue.path, message: issue.message});
+        console.log(dbResult);
+        if (dbResult.length > 0 && dbResult[0].length === 0) {
+          result = { status: true, data: dbResult[1] };
+        } else {
+          let errorState: { path: (string | number)[]; message: string }[] = [];
+          dbResult[0].forEach((error: any) => {
+            errorState.push({
+              path: [error.error_path],
+              message: error.error_text,
+            });
+          });
+          result = {
+            status: false,
+            data: errorState,
+          };
         }
-        result = {status: false, data:errorState };
+      } else {
+        let errorState: { path: (string | number)[]; message: string }[] = [];
+        for (const issue of parsed.error.issues) {
+          errorState.push({ path: issue.path, message: issue.message });
+        }
+        result = { status: false, data: errorState };
         return result;
       }
     } else {
-      result = {status: false, data: [{path:["form"], message:"Error: Server Error"}] };
+      result = {
+        status: false,
+        data: [{ path: ["form"], message: "Error: Server Error" }],
+      };
     }
     return result;
   } catch (e: any) {
     console.log(e);
-    if ((e instanceof SqlError) && e.code === 'ER_DUP_ENTRY' ) {
-      result = {status: false, data: [{path:["name"], message:"Error: Value already exist"}] };
+    if (e instanceof SqlError && e.code === "ER_DUP_ENTRY") {
+      result = {
+        status: false,
+        data: [{ path: ["name"], message: "Error: Value already exist" }],
+      };
       return result;
     }
   }
-  result = {status: false, data: [{path:["form"], message:"Error: Unknown Error"}] };
+  result = {
+    status: false,
+    data: [{ path: ["form"], message: "Error: Unknown Error" }],
+  };
   return result;
 }
 
+export async function updateContact(data: contactSchemaT) {
+  let result;
+  try {
+    const session = await getSession();
+    if (session) {
+      const parsed = zs.contactSchema.safeParse(data);
+      if (parsed.success) {
+        const dbResult = await updateContactDB(session, data as contactSchemaT);
+        if (dbResult.length > 0 && dbResult[0].length === 0) {
+          result = { status: true, data: dbResult[1] };
+        } else {
+          let errorState: { path: (string | number)[]; message: string }[] = [];
+          dbResult[0].forEach((error: any) => {
+            errorState.push({
+              path: [error.error_path],
+              message: error.error_text,
+            });
+          });
+          result = {
+            status: false,
+            data: errorState,
+          };
+        }
+      } else {
+        let errorState: { path: (string | number)[]; message: string }[] = [];
+        for (const issue of parsed.error.issues) {
+          errorState.push({ path: issue.path, message: issue.message });
+        }
+        result = { status: false, data: errorState };
+        return result;
+      }
+    } else {
+      result = {
+        status: false,
+        data: [{ path: ["form"], message: "Error: Server Error" }],
+      };
+    }
+    return result;
+  } catch (e: any) {
+    console.log(e);
+    if (e instanceof SqlError && e.code === "ER_DUP_ENTRY") {
+      result = {
+        status: false,
+        data: [{ path: ["name"], message: "Error: Value already exist" }],
+      };
+      return result;
+    }
+  }
+  result = {
+    status: false,
+    data: [{ path: ["form"], message: "Error: Unknown Error" }],
+  };
+  return result;
+}
 
 /**
- * 
+ *
  * @param searchString partial string for contact name
- * @returns 
+ * @returns
  */
 export async function getContact(searchString: string) {
   try {
@@ -64,9 +138,9 @@ export async function getContact(searchString: string) {
 }
 
 /**
- * 
+ *
  * @param Id id of the contact to be searched
- * @returns 
+ * @returns
  */
 export async function getContactById(id: string) {
   try {
