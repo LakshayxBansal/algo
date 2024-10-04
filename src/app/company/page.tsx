f436dcd89b3270bf9adeff574c3ab0a45784aaa2
ankit <ankit.chaudhary@algofast.in>
2024-10-04 19:04:19 +0530

    api routes added

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -1,23 +1,21 @@
-import * as React from 'react';
-import { getSession }  from '../services/session.service';
-import { redirect } from 'next/navigation';
-import CompanyEntityList from './CompanyEntityList';
-import InviteEntityList from './InviteEntityList';
-import { Box } from '@mui/material';
+import * as React from "react";
+import { getSession } from "../services/session.service";
+import { redirect } from "next/navigation";
+import CompanyEntityList from "./CompanyEntityList";
+import InviteEntityList from "./InviteEntityList";
+import { Box } from "@mui/material";
 
 export default async function Companies() {
   const session = await getSession();
-  console.log(session);
   if (session) {
-    
     return (
       <>
-      <Box>
-        <CompanyEntityList/>
-      </Box>
-      <Box sx={{mt: 12}}>
-        <InviteEntityList/>
-      </Box>
+        <Box>
+          <CompanyEntityList />
+        </Box>
+        <Box sx={{ mt: 12 }}>
+          <InviteEntityList />
+        </Box>
       </>
     );
   } else {
