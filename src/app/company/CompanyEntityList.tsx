cb949fa042898242dd1be772fa753ccf3e601941
ChaitanyaAlgo07 <chaitanya.ratta@algofast.in>
2024-10-11 17:35:43 +0530

    loader issues fix

--- src/app/company/CompanyEntityList.tsx
+++ src/app/company/CompanyEntityList.tsx
@@ -1,57 +1,64 @@
-"use client"
+"use client";
 import { GridColDef } from "@mui/x-data-grid";
-import { getCompanies, getCompanyById } from "../controllers/company.controller"
-import EntityList from "../Widgets/masters/EntityList"
-import CreateCompany from "./CreateCompany"
+import {
+  getCompanies,
+  getCompanyById,
+} from "../controllers/company.controller";
+import EntityList from "../Widgets/masters/EntityList";
+import CreateCompany from "./CreateCompany";
 import CellDbName from "./cellDBName";
 import AuthWrapper from "./AuthWrapper";
+import { Typography } from "@mui/material";
 
-export default function CompanyEntityList(){
-    const columns: GridColDef[] = [
-        { field: 'RowID', headerName: 'ID', width: 90 },
-        {
-          field: 'companyName',
-          headerName: 'Name',
-          width: 150,
-          renderCell: (params) => (
-            <AuthWrapper>
-              <CellDbName
-                row={params.row}
-                userId={params.row.userId as number}
-              ></CellDbName>
-            </AuthWrapper>
-          ),
-        },
-        {
-          field: 'companyAlias',
-          headerName: 'Alias',
-          width: 150,
-        },
-        {
-          field: 'createdBy',
-          headerName: 'Created By',
-          width: 150,
-        },
-        {
-          field: 'createdOn',
-          headerName: 'Created On',
-          width: 150,
-        }
-      ];
-    return <>
-        <EntityList
+export default function CompanyEntityList() {
+  const columns: GridColDef[] = [
+    { field: "RowID", headerName: "ID", width: 90 },
+    {
+      field: "companyName",
+      headerName: "Name",
+      width: 150,
+      renderCell: (params) => (
+        <AuthWrapper>
+          <CellDbName
+            row={params.row}
+            userId={params.row.userId as number}
+          ></CellDbName>
+        </AuthWrapper>
+      ),
+    },
+    {
+      field: "companyAlias",
+      headerName: "Alias",
+      width: 150,
+    },
+    {
+      field: "createdBy",
+      headerName: "Created By",
+      width: 150,
+    },
+    {
+      field: "createdOn",
+      headerName: "Created On",
+      width: 150,
+    },
+  ];
+  return (
+    <>
+      <Typography variant="h6">Company List</Typography>
+      <EntityList
         title="Company List"
         renderForm={(fnDialogOpen, fnDialogValue, data) => (
           <CreateCompany
-          setDialogOpen={fnDialogOpen}
-          setDialogValue={fnDialogValue}
-          data={data}
+            setDialogOpen={fnDialogOpen}
+            setDialogValue={fnDialogValue}
+            data={data}
           />
-        )} 
+        )}
         fetchDataFn={getCompanies}
         fnFetchDataByID={getCompanyById}
         customCols={columns}
-        AddAllowed={true}>
-      </EntityList>
-      </>
+        AddAllowed={true}
+      ></EntityList>
+    </>
+  );
 }
