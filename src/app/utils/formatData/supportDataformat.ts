bcd106ff0bfe2f6aa8f4537932fcd509e4b34c0e
Rishabh-algofast <rishabh.aggarwal@algofast.in>
2024-11-15 12:41:41 +0530

    bug made other data optional

--- src/app/utils/formatData/supportDataformat.ts
+++ src/app/utils/formatData/supportDataformat.ts
@@ -19,7 +19,7 @@ export async function supportDataFormat({
   const nextActionDate = formatDate(formData.get("next_action_date") as string);
 
   const headerData = {
-    tkt_number: formData.get("tkt_number") as string ?? otherData.tkt_number,
+    tkt_number: formData.get("tkt_number") as string ?? otherData?.tkt_number,
     date,
     contact_id: selectValues.contact?.id,
     received_by_id: selectValues.received_by?.id,
@@ -27,7 +27,7 @@ export async function supportDataFormat({
     contact: selectValues.contact?.name ?? "",
     received_by: selectValues.received_by?.name ?? "",
     category: selectValues.category?.name ?? "",
-    call_receipt_remark: (formData.get("call_receipt_remark") ?? "") as string ?? otherData.call_receipt_remark,
+    call_receipt_remark: (formData.get("call_receipt_remark") ?? "") as string ?? otherData?.call_receipt_remark,
   };
 
   let ledgerData = {
@@ -40,9 +40,9 @@ export async function supportDataFormat({
     next_action_id: selectValues.next_action?.id,
     next_action: selectValues.next_action?.name ?? "",
     next_action_date: nextActionDate,
-    suggested_action_remark: (formData.get("suggested_action_remark")?? "" ) as string ?? otherData.suggested_action_remark,
-    action_taken_remark: (formData.get("action_taken_remark")) as string ?? otherData.action_taken_remark,
-    closure_remark: (formData.get("closure_remark") ?? "") as string ?? otherData.closure_remark,
+    suggested_action_remark: (formData.get("suggested_action_remark")?? "" ) as string ?? otherData?.suggested_action_remark,
+    action_taken_remark: (formData.get("action_taken_remark" )?? "") as string ,
+    closure_remark: (formData.get("closure_remark") ?? "") as string ?? otherData?.closure_remark,
   };
 
   return { ...headerData, ...ledgerData};
