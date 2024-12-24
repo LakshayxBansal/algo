dfc755574cb04b377caf50ec7919c3e7a25403c5
harshalgofast <harsh.khandelwal@algofast.in>
2024-12-24 18:57:34 +0530

    UI Redesigned for Login Page

--- src/app/company/loading.tsx
+++ src/app/company/loading.tsx
@@ -1,10 +1,37 @@
-// loading.tsx
-import React from "react";
+// // loading.tsx
+// import React from "react";
 
-export default function Loading() {
-  return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <p>Loading...</p>
-    </div>
-  );
+// export default function Loading() {
+//   return (
+//     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+//       <p>Loading...</p>
+//     </div>
+//   );
+// };
+
+
+
+"use client"
+import { useEffect } from "react";
+import { ReactNode } from "react";
+
+interface TransitionLinkProps{
+    children: ReactNode;
+    // href:string;
+}
+export default function Loading({children}:TransitionLinkProps){
+
+    useEffect(()=>{
+
+        document.body.classList.add('cursor-wait');
+        return () => {
+            document.body.classList.remove('cursor-wait');
+        };
+    },[]);
+
+    return(
+        <div>
+            {children}
+        </div>
+    )
 }
