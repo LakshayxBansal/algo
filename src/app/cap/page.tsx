931c1725a67bbf69bfa476c8374f670aa795d7c0
ankitPandey-json-3 <ankit.pandey@algofast.in>
2025-01-13 11:22:03 +0530

    #make the dashboard aligned with controllers

--- src/app/cap/page.tsx
+++ src/app/cap/page.tsx
@@ -3,17 +3,23 @@ import Dashbaord  from './dashboard/page';
 import {  LinearProgress } from "@mui/material";
 import TabLayout from "./TabLayout";
 import SupportDashboard from "./supportDashboard/page";
+import { getConfigData } from "../controllers/enquiry.controller";
 
 export default async function ClientApp() {
 
   const dashboardContent = await Dashbaord();
   const supportDashboardContent = await SupportDashboard();
-
+  const resultEnquiry = await getConfigData('enquiry');
+  const enquiryEnabled = JSON.parse(resultEnquiry[0].config).reqd;
+  const resultSupport = await getConfigData('support');
+  const supportEnabled = JSON.parse(resultSupport[0].config).reqd;
   return (
     <Suspense fallback={<LinearProgress/>}>
       <TabLayout 
         dashboardContent={dashboardContent}
         supportDashboardContent={supportDashboardContent}
+        enquiryEnabled={enquiryEnabled}
+        supportEnabled={supportEnabled}
       />
     </Suspense>
   );
