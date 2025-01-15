15f0880178128b4681dd13df307dc940ac44120b
Ankitalgofast <ankit.chaudhary@algofast.in>
2025-01-15 17:45:58 +0530

    firebase util update

--- src/app/utils/firebase.utils.ts
+++ src/app/utils/firebase.utils.ts
@@ -4,6 +4,7 @@ export function initializeApp() {
     if (!admin.apps.length) {        
         admin.initializeApp({
           credential: admin.credential.applicationDefault(),
+          projectId: "algofast-crm-prod"
         });
     }
 //   admin.initializeApp({
